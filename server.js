3753e3f4a791e958adabba75a95f7d3c25b4f9f2
David Cordova <d.mcordova87@gmail.com>
2018-12-28 23:46:06 -0700

    heroku deploy prep

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
+
 const items = require('./routes/api/items');
 
 const app = express();
@@ -24,6 +26,16 @@ mongoose
 
 // User Routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
